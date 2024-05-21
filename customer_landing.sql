fd727dbd0eb2180fa172b019e5c612b0cc6bf876
Theresa <91969220+theresaxx@users.noreply.github.com>
2024-05-21 19:39:24 +0200

    Create customer_landing.sql

---
+++ customer_landing.sql
@@ -0,0 +1,21 @@
+CREATE EXTERNAL TABLE `customer_landing`(
+  `serialnumber` string COMMENT 'from deserializer', 
+  `sharewithpublicasofdate` bigint COMMENT 'from deserializer', 
+  `birthday` string COMMENT 'from deserializer', 
+  `registrationdate` bigint COMMENT 'from deserializer', 
+  `sharewithresearchasofdate` bigint COMMENT 'from deserializer', 
+  `customername` string COMMENT 'from deserializer', 
+  `email` string COMMENT 'from deserializer', 
+  `lastupdatedate` bigint COMMENT 'from deserializer', 
+  `phone` string COMMENT 'from deserializer', 
+  `sharewithfriendsasofdate` bigint COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://stedi-landing-zone/customer/landing/'
+TBLPROPERTIES (
+  'classification'='json')
